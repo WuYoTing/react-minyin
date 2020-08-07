e692050cfaf7eb60b0f8f9b2802a5ca9b0de78e7
WuTim0307 <upk.rd00003@gmail.com>
2020-08-07 13:11:57 +0800

    Updates

---
+++ precache-manifest.f9f0b51aa2fa0748d970b57f2e7b2ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf58a4ac9dbadaf753722a7116d17bc5",
+    "url": "/minyin/index.html"
+  },
+  {
+    "revision": "72ad9b90c99aa114685a",
+    "url": "/minyin/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "f7cec23afbb4d2098572",
+    "url": "/minyin/static/css/main.6b076e44.chunk.css"
+  },
+  {
+    "revision": "72ad9b90c99aa114685a",
+    "url": "/minyin/static/js/2.1c646bd5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/minyin/static/js/2.1c646bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cec23afbb4d2098572",
+    "url": "/minyin/static/js/main.af993b95.chunk.js"
+  },
+  {
+    "revision": "ac165f88e6c513256282",
+    "url": "/minyin/static/js/runtime-main.b7e4ab9a.js"
+  }
+]);
