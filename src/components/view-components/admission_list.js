92ad80c700262572466f838ca17084840f0ce6b7
WuTim0307 <upk.rd00003@gmail.com>
2020-08-10 17:19:22 +0800

    feat: 新增部分跑馬燈圖片

--- src/components/view-components/admission_list.js
+++ src/components/view-components/admission_list.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "./view_components.scss";
 import Image from "react-bootstrap/Image";
+import Carousel from "react-bootstrap/Carousel";
 
 
 const admission_list = () => {
     return (
         <div className='mt-3 text-center'>
-            <Image src="/images/admission_list.jpg" responsive/>
+            <div className='mt-3 text-center'>
+                <Carousel className="admission_list">
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/admission_list_1.jpg"
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/admission_list_2.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/admission_list_3.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/admission_list_persion_1.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/images/admission_list_persion_2.jpg"
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     )
 }
--- src/components/view-components/home.js
+++ src/components/view-components/home.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
 
 const home = () => {
     return (
         <div className='mt-3 text-center'>
-
+            <Carousel className="home">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/images/main_entrance.jpg"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/images/home_intro_1.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/images/home_intro_2.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/images/home_intro_3.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
